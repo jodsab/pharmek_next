017c0373b9de002f8cfb51c746dbc001b36c62f5
jodsab <camilo.b.q@hotmail.com>
2024-03-19 17:06:02 -0500

    new products

--- src/core/productsSlider.js
+++ src/core/productsSlider.js
@@ -120,9 +120,20 @@ const BABYS_ANIMALS = [
   },
   {
     id: 3,
-    nombre: "Pracanex jeringa 2 mL",
+    nombre: "Pracanex jeringa",
     inserto: "insertos/INSERTO PRACANEX.pdf",
-    src: "img/antiparasitario-interno/Pracanex 2 ml.png",
+    src: [
+      "img/antiparasitario-interno/Pracanex 2 ml.png",
+      "img/antiparasitario-interno/3D Pracanex Suspension hoja venta 2.png",
+      "img/antiparasitario-interno/3D Pracanex Suspension Jeringas 3 modelos.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Pracanex jeringa",
+        src: "img/antiparasitario-interno/Pracanex 2 ml.png",
+      },
+    ],
     content: (
       <div className="contenido">
         <p>
@@ -236,6 +247,554 @@ const BABYS_ANIMALS = [
         </p>
         <p>
           <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 6,
+    nombre: "Afomax",
+    src: ["img/news/3D Afomax.png"],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Afomax",
+        src: "img/news/3D Afomax.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Lorem Ipsum is simply
+          dummy text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 7,
+    nombre: "Amino Max",
+    src: [
+      "img/news/Amino max 3D 10ml.png",
+      "img/news/Amino Max F 20mL 3D.png",
+      "img/news/Amino Max F 50mL 3D.png",
+      "img/news/Amino Max F 100 mL 3D.png",
+      "img/news/Amino Max F 250 mL 3D.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Amino Max 10ml",
+        src: "img/news/Amino max 3D 10ml.png",
+      },
+      {
+        id: 2,
+        nombre: "Amino Max 20ml",
+        src: "img/news/Amino Max F 20mL 3D.png",
+      },
+      {
+        id: 3,
+        nombre: "Amino Max 50ml",
+        src: "img/news/Amino Max F 50mL 3D.png",
+      },
+      {
+        id: 4,
+        nombre: "Amino Max 100ml",
+        src: "img/news/Amino Max F 100 mL 3D.png",
+      },
+      {
+        id: 5,
+        nombre: "Amino Max 250ml",
+        src: "img/news/Amino Max F 250 mL 3D.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Lorem Ipsum is simply
+          dummy text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 8,
+    nombre: "Duomec F",
+    src: [
+      "img/news/3D Duomec 10 ml.png",
+      "img/news/3D Duomec F  20 mL.png",
+      "img/news/3D Duomec F  50 mL.png",
+      "img/news/3D Duomec F 100 mL.png",
+      "img/news/3D Duomec F 250 mL.png",
+      "img/news/3D Duomec F 500 mL.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Duomec F 10 mL",
+        src: "img/news/3D Duomec 10 ml.png",
+      },
+      {
+        id: 2,
+        nombre: "Duomec F 20 mL",
+        src: "img/news/3D Duomec F  20 mL.png",
+      },
+      {
+        id: 3,
+        nombre: "Duomec F 50 mL",
+        src: "img/news/3D Duomec F  50 mL.png",
+      },
+      {
+        id: 4,
+        nombre: "Duomec F 100 mL",
+        src: "img/news/3D Duomec F 100 mL.png",
+      },
+      {
+        id: 5,
+        nombre: "Duomec F 250 mL",
+        src: "img/news/3D Duomec F 250 mL.png",
+      },
+      {
+        id: 6,
+        nombre: "Duomec F 500 mL",
+        src: "img/news/3D Duomec F 500 mL.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Ivermectina 10 mg, clorsulón 100 mg,
+          excipientes c.s.p. 1 mL
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Para el tratamiento, control y
+          profilaxis de parasitosis internas y externas, incluyendo Fasciola
+          hepatica, nematodos gastrointestinales y pulmonares,además de
+          ectoparásitos como: estados larvarios de dípteros causantes de
+          miasis,piojos chupadores y masticadores, garrapatas y ácaros
+          productores de sarna.
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>1 mL/50 kg de p.v.
+          Aplicar vía subcutánea.
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Frasco x
+          20mL,50mL,100mL,250mL,500mL
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 9,
+    nombre: "Ectofinex Duo",
+    src: [
+      "img/news/3D Ectofinex Duo  110 mL.png",
+      "img/news/3D Ectofinex Duo  275 mL.png",
+      "img/news/Ectofinex Duo 500 mL 3D.png",
+      "img/news/3D Ectofinex Duo  55 mL.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Ectofinex Duo 110ml",
+        src: "img/news/3D Ectofinex Duo  110 mL.png",
+      },
+      {
+        id: 2,
+        nombre: "Ectofinex Duo 275ml",
+        src: "img/news/3D Ectofinex Duo  275 mL.png",
+      },
+      {
+        id: 3,
+        nombre: "Ectofinex Duo 500ml",
+        src: "img/news/Ectofinex Duo 500 mL 3D.png",
+      },
+      {
+        id: 4,
+        nombre: "Ectofinex Duo 55ml",
+        src: "img/news/3D Ectofinex Duo  55 mL.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Lorem Ipsum is simply
+          dummy text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 10,
+    nombre: "Ectofinex",
+    src: [
+      "img/news/Ectofinex AF  110 mL 3D.png",
+      "img/news/Ectofinex 275mL 3D.png",
+      "img/news/Ectofinex 500mL 3D.png",
+      "img/news/Ectofinex 55 mL 3D.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Ectofinex AF  110 mL",
+        src: "img/news/Ectofinex AF  110 mL 3D.png",
+      },
+      {
+        id: 2,
+        nombre: "Ectofinex AF  275 mL",
+        src: "img/news/Ectofinex 275mL 3D.png",
+      },
+      {
+        id: 3,
+        nombre: "Ectofinex AF  500 mL",
+        src: "img/news/Ectofinex 500mL 3D.png",
+      },
+      {
+        id: 4,
+        nombre: "Ectofinex AF  55 mL",
+        src: "img/news/Ectofinex 55 mL 3D.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Lorem Ipsum is simply
+          dummy text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 11,
+    nombre: "Oxigan",
+    src: [
+      "img/news/3D Oxigan 20mL.png",
+      "img/news/3D Oxigan 50mL.png",
+      "img/news/3D Oxigan 100 mL.png",
+      "img/news/3D Oxigan 250mL.png",
+      "img/news/3D Oxigan 10 mL.png",
+      "img/news/3D Oxigan 500mL.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Oxigan 20mL",
+        src: "img/news/3D Oxigan 20mL.png",
+      },
+      {
+        id: 2,
+        nombre: "Oxigan 50mL",
+        src: "img/news/3D Oxigan 50mL.png",
+      },
+      {
+        id: 3,
+        nombre: "Oxigan 100mL",
+        src: "img/news/3D Oxigan 100 mL.png",
+      },
+      {
+        id: 4,
+        nombre: "Oxigan 250mL",
+        src: "img/news/3D Oxigan 250mL.png",
+      },
+      {
+        id: 5,
+        nombre: "Oxigan 10mL",
+        src: "img/news/3D Oxigan 10 mL.png",
+      },
+      {
+        id: 6,
+        nombre: "Oxigan 500mL",
+        src: "img/news/3D Oxigan 500mL.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Lorem Ipsum is simply
+          dummy text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 12,
+    nombre: "Sedaket",
+    src: ["img/news/3D SEDAKET 100 mL.png", "img/news/Sedaket 3D 50 mL.png"],
+    presentation: [
+      {
+        id: 1,
+        nombre: "SEDAKET 100 mL",
+        src: "img/news/3D SEDAKET 100 mL.png",
+      },
+      {
+        id: 2,
+        nombre: "Sedaket 50 mL",
+        src: "img/news/Sedaket 3D 50 mL.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Ketamina (clorhidrato) 100 mg,
+          excipientes c.s.p. 1 mL.
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Para la pre-medicación, inducción y
+          mantenimiento de la anestesia.Se puede usar combinado con otras drogas
+          inyectables o inhalatorias, obteniendo un mejor resultado anestésico.
+          En anestesias generales cortas, para producir una inmovilización del
+          paciente con la finalidad de realizar maniobras quirúrgicas breves o
+          procedimientos diagnósticos menores. En anestesias de pacientes con
+          riesgo aumentado ya que no compromete el sistema cardiorrespiratorio
+          del animal. De elección en cesáreas, debido a la ausencia de toxicidad
+          fetal
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Caninos: 5 - 15 mg/kg
+          vía intravenosa o de 15 - 25mg/kg vía intramuscular; felinos: 5 - 8
+          mg/kg vía intravenosa o de 22 - 33 mg/kg vía intramuscular; equinos: 2
+          - 5 mg/kg vía intravenosa o de 8 - 15 mg/kg vía intramuscular;
+          bovinos: 2 - 5 mg/kg vía intravenosa, 15 mg/kg vía intramuscular o 2
+          mg/kg vía epidural; porcinos: 2 - 5 mg/kg vía intravenosa o de 15 - 20
+          mg/kg vía intramuscular;ovinos: 2 - 5 mg/kg vía intravenosa o de 10 a
+          20 mg/kg vía intramuscular; aves: 12 -70 mg/kg vía intramuscular.
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Frasco x 50mL,100mL
+        </p>
+        <p>
+          <strong>Registro senasa</strong>
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 13,
+    nombre: "Toldinfosfan",
+    src: [
+      "img/news/3D Toldifosfan 500 mL.png",
+      "img/news/3D Toldifosfan 250 mL.png",
+      "img/news/3D Toldinfosfan 100 mL.png",
+      "img/news/3D Toldifosfan 50 mL.png",
+      "img/news/3D Toldifosfan 20 mL.png",
+      "img/news/3D Toldifosfan 10 mL.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Toldifosfan 500 mL",
+        src: "img/news/3D Toldifosfan 500 mL.png",
+      },
+      {
+        id: 2,
+        nombre: "Toldifosfan 250 mL",
+        src: "img/news/3D Toldifosfan 250 mL.png",
+      },
+      {
+        id: 3,
+        nombre: "Toldifosfan 100 mL",
+        src: "img/news/3D Toldinfosfan 100 mL.png",
+      },
+      {
+        id: 4,
+        nombre: "Toldifosfan 50 mL",
+        src: "img/news/3D Toldifosfan 50 mL.png",
+      },
+      {
+        id: 5,
+        nombre: "Toldifosfan 20 mL",
+        src: "img/news/3D Toldifosfan 20 mL.png",
+      },
+      {
+        id: 6,
+        nombre: "Toldifosfan 10 mL",
+        src: "img/news/3D Toldifosfan 10 mL.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Cada mL contiene Toldimfos sódico 200 mg
+          Sulfato de zinc 1.1 mg Selenito de sodio 0.33 mg Ácido nicotínico 5 mg
+          Excipientes c.s.p 1mL
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Es una solución multimineral, compuesta
+          por fuentes de Fósforo , Zinc , Selenio , y Ácido Nicotínico, de uso
+          parenteral, indicado en Bovinos, Equinos, Porcinos, Caprinos, Ovinos y
+          Caninos, en procesos carenciales de sus componentes, los cuales son de
+          vital trascendencia en los procesos, inmunológicos, productivos y
+          reproductivos. Adicionalmente al corregir las deficiencias de
+          minerales, corrige alteraciones reproductivas presentadas por los
+          machos desde la fertilización, bien sea por Inseminación Artificial o
+          Monta directa, como en las hembras durante el ciclo estral, parto y
+          lactancia.
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Aplicar 1 mL/20 kg de
+          p.v. intramuscular profunda, volúmenes mayores a 10 mL deben ser
+          repartidos.
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Frasco de vidrio x
+          20mL,50mL,100mL,250mL,500Ml
+        </p>
+        <p>
+          <strong>Registro senasa</strong> F.75.01.N.0136
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: 14,
+    nombre: "Trak ADE",
+    src: [
+      "img/trak-ade/3D Trak ADE 500 mL.png",
+      "img/trak-ade/3D Trak ADE 250 mL.png",
+      "img/trak-ade/3D TRAK ADE 100 mL.png",
+      "img/trak-ade/3D Trak ADE 50 mL.png",
+      "img/trak-ade/3D Trak ACE 20 mL.png",
+      "img/trak-ade/3D Trak ACE 10 mL.png",
+    ],
+    presentation: [
+      {
+        id: 1,
+        nombre: "Trak ADE 500 mL",
+        src: "img/trak-ade/3D Trak ADE 500 mL.png",
+      },
+      {
+        id: 2,
+        nombre: "Trak ADE 250 mL",
+        src: "img/trak-ade/3D Trak ADE 250 mL.png",
+      },
+      {
+        id: 3,
+        nombre: "Trak ADE 100 mL",
+        src: "img/trak-ade/3D TRAK ADE 100 mL.png",
+      },
+      {
+        id: 4,
+        nombre: "Trak ADE 50 mL",
+        src: "img/trak-ade/3D Trak ADE 50 mL.png",
+      },
+      {
+        id: 5,
+        nombre: "Trak ADE 20 mL",
+        src: "img/trak-ade/3D Trak ACE 20 mL.png",
+      },
+      {
+        id: 6,
+        nombre: "Trak ADE 10 mL",
+        src: "img/trak-ade/3D Trak ACE 10 mL.png",
+      },
+    ],
+    content: (
+      <div className="contenido">
+        <p>
+          <strong>Composición: </strong>Cada mL Ivermectina 10 mg, Vitamina A
+          250 000 UI, Vitamina D3 37 500 UI, Vitamina E (alfatocoferil acetato)
+          25 mg, vehículo de lenta liberación c.s.p. 1mL
+        </p>
+        <p>
+          <strong>Indicaciones: </strong>Para el tratamiento y control de
+          nematodos causantes del parasitismo gastrointestinal y pulmonar en
+          bovinos y porcinos.También para el control de Dermatobia hominis
+          (Nuche o Tórsalo), Cochliomya hominivorax (Larvas de miasis
+          ogusaneras), al igual que Piojos, Ácaros productores de Sarna, y
+          Garrapata del género Boophillus microplus. Es el ideal para el control
+          de gusaneras por heridas de castraciones y ombligos de terneros.
+        </p>
+        <p>
+          <strong>Dosis y vías de administración: </strong>Aplicar por vía
+          intramuscular o subcutánea - Bovinos, ovinos, caprinos y camélidos
+          sudamericanos: 1 ml / 50 kg de peso. (200 μg Ivermectina/kg de peso) -
+          Porcinos: 1 ml / 33 kg de peso (300 μg Ivermectina/kg de peso).
+        </p>
+        <p>
+          <strong>Presentación comercial: </strong>Frasco de vidrio x
+          10ml,20ml,50ml,100ml,250ml,500ml
+        </p>
+        <p>
+          <strong>Registro senasa</strong>F.54.01.N.0242
         </p>
       </div>
     ),
@@ -462,38 +1021,38 @@ const ADULTS_ANIMALS = [
   },
   {
     id: 4,
-    nombre: "Track 50 mL",
+    nombre: "Trak 50 mL",
     inserto: "insertos/TRAK-LA_INSERTO.pdf",
     filter: ["Antiparasitarios internos y externos"],
     src: "img/antiparasitario-interno-y-externo/Trak 50 ml.png",
     presentation: [
       {
         id: 1,
-        nombre: "Track 20 mL",
+        nombre: "Trak 20 mL",
         src: "img/antiparasitario-interno-y-externo/Trak 20 ml.png",
         inserto: "insertos/TRAK-LA_INSERTO.pdf",
       },
       {
         id: 2,
-        nombre: "Track 50 mL",
+        nombre: "Trak 50 mL",
         src: "img/antiparasitario-interno-y-externo/Trak 50 ml (1).png",
         inserto: "insertos/TRAK-LA_INSERTO.pdf",
       },
       {
         id: 3,
-        nombre: "Track 100 mL",
+        nombre: "Trak 100 mL",
         src: "img/antiparasitario-interno-y-externo/Trak 100 ml.png",
         inserto: "insertos/TRAK-LA_INSERTO.pdf",
       },
       {
         id: 4,
-        nombre: "Track 250 mL",
+        nombre: "Trak 250 mL",
         src: "img/antiparasitario-interno-y-externo/Trak 250 ml.png",
         inserto: "insertos/TRAK-LA_INSERTO.pdf",
       },
       {
         id: 5,
-        nombre: "Track 500 mL",
+        nombre: "Trak 500 mL",
         src: "img/antiparasitario-interno-y-externo/Trak 500 ml.png",
         inserto: "insertos/TRAK-LA_INSERTO.pdf",
       },
