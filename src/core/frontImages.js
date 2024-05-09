f206b06ea90559640235b61cd42b2a422d668206
jodsab <camilo.b.q@hotmail.com>
2024-05-08 21:00:39 -0500

    portadas

--- src/core/frontImages.js
+++ src/core/frontImages.js
@@ -1,12 +1,14 @@
 const IMG_DESKTOP = [
-  { id: 1, src: "fondo-portada.jpg" },
-  { id: 2, src: "foto-publicidad-pracanex.jpg" },
-  { id: 3, src: "dermicare-banner-pc.jpg" },
+  { id: 1, src: "portada1.jpeg" },
+  { id: 2, src: "portada2.jpeg" },
+  { id: 3, src: "portada3.jpeg" },
+  { id: 4, src: "foto-publicidad-pracanex.jpg" },
+  { id: 5, src: "dermicare-banner-pc.jpg" },
 ];
 const IMG_MOBILE = [
-  { id: 1, src: "portadamv.jpg" },
-  { id: 2, src: "movil.jpg" },
-  { id: 3, src: "dermicare-banner-mv.jpg" },
+  { id: 4, src: "portadamv.jpg" },
+  { id: 5, src: "movil.jpg" },
+  { id: 6, src: "dermicare-banner-mv.jpg" },
 ];
 
 export { IMG_DESKTOP, IMG_MOBILE };
